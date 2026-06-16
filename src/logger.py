d56194c79728b999674adbe4b61d500bdc4a7d72
DavidYe68 <30755977@qq.com>
2026-06-17 01:28:16 +0800

    增加log记录内容

--- src/logger.py
+++ src/logger.py
@@ -3,15 +3,26 @@ import os
 from datetime import datetime
 
 class Logger():
-    def __init__(self):
+    def __init__(self, case_id=""):
+        self.case_id = case_id
         self.logs = []
     
-    def log(self, agent_name, input_data, output_data):
+    def log(self, agent_name, input_data, output_data, round_id="", 
+            start_time="", end_time="",  latency_sec=None, retry_count=0, 
+            validation_passed=True,error=""):
         entry = {
             "agent_name": agent_name,
             "timestamp": datetime.now().isoformat(),
             "input": input_data,
-            "output": output_data
+            "output": output_data,
+            "case_id": self.case_id,
+            "round_id": round_id,
+            "start_time": start_time,
+            "end_time": end_time,
+            "latency_sec": latency_sec,
+            "retry_count": retry_count,
+            "validation_passed": validation_passed,
+            "error": error,
         }
 
         self.logs.append(entry)
